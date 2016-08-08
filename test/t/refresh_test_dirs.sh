45434033b8748eddda7e4f29ad499dace5dd9d66
Christopher Bottoms <molecules@cpan.org>
2016-08-08 13:32:14 -0500

    Not working with gzipped files, for now

--- test/t/refresh_test_dirs.sh
+++ test/t/refresh_test_dirs.sh
@@ -1,9 +1,11 @@
 if [ -f t/test_results.log ]
 then
-    echo "Removing old test restuls and resetting test directories"
+    echo "Removing old test results and resetting test directories"
     rm t/test_results.log
     rm -rf TestData_?
-
+    
     echo "Checking out test data directories from git"
     git checkout TestData_1 TestData_2 TestData_3
+
+    # cp -r gzipped_TestData_3.orig TestData_3_gzipped
 fi
